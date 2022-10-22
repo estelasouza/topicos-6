e88ba76a7d8c698312be3c2d7c9bfcda7083eff5
Maria Estela <maria.estela@ifood.com.br>
2022-10-22 14:09:53 -0300

    get messa on tww

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
 const { TwitterApi } = require('twitter-api-v2');
 
+const Twitter = require('twitter-v2');
+
 
 // KC1ZM0Z3MJa0ZIFffpbeEUzeY - api key
 //qdjscfqKdWAX7PG5FMkqOGUkNva4qfPaeQ5akDbu49CTH7T8xd - api key secret 
 
-const userClient = new TwitterApi({
-    appKey: "KC1ZM0Z3MJa0ZIFffpbeEUzeY",
-    appSecret: "qdjscfqKdWAX7PG5FMkqOGUkNva4qfPaeQ5akDbu49CTH7T8xd"
-
-})
+const client = new Twitter({
+    bearer_token: 'AAAAAAAAAAAAAAAAAAAAAKHWiQEAAAAAm6CcT680F4dfQTi9URNdZ04ZyCg%3DV6PmWkXvzg784Os78Sdf3CCK7BYRKBhgd7k0Z7e5Zl8oUkwRWZ',
+  });
 
 
 async function test(){
-
-    await userClient.v1.tweet("hello, this is a test");
+    
+    const { data } = await client.get('tweets', { ids: '1228393702244134912' });
+    console.log(data);
 }
 
 test()
