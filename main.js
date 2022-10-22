40c019dc59e91e846ce344c73d22d45390271817
Maria Estela <maria.estela@ifood.com.br>
2022-10-22 16:08:15 -0300

    send tweet

--- main.js
+++ main.js
@@ -1,20 +1,27 @@
 const { TwitterApi } = require('twitter-api-v2');
+require('dotenv').config();
 
-const Twitter = require('twitter-v2');
 
-
-// KC1ZM0Z3MJa0ZIFffpbeEUzeY - api key
-//qdjscfqKdWAX7PG5FMkqOGUkNva4qfPaeQ5akDbu49CTH7T8xd - api key secret 
-
-const client = new Twitter({
-    bearer_token: 'AAAAAAAAAAAAAAAAAAAAAKHWiQEAAAAAm6CcT680F4dfQTi9URNdZ04ZyCg%3DV6PmWkXvzg784Os78Sdf3CCK7BYRKBhgd7k0Z7e5Zl8oUkwRWZ',
+const client = new TwitterApi({
+    appKey:process.env.APP_KEY,
+    appSecret:process.env.APP_SECRET,
+    accessToken:process.env.ACCESS_TOKEN,
+    accessSecret: process.env.ACCESS_SECRET
   });
 
+  
+  async function test(){
+  
 
-async function test(){
+    const mediaIds = await Promise.all([
+        // file path
+        client.v1.uploadMedia('./fluxograma.drawio.png'),
+      ]);
+      
+      // mediaIds is a string[], can be given to .tweet
+      await client.v1.tweet('My tweet text with two images!', { media_ids: mediaIds });
     
-    const { data } = await client.post('tweets',{ text: "hello"})
-      console.log(data)
+
 }
 
 test()
